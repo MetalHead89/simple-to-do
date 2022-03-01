ddae5353e88d317406e2644ed0c94773857ef898
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-03-01 21:39:01 +0700

    Добавлен компонент List

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import List from '../List/List';
 import './App.scss';
 
 function App() {
   return (
     <div className="app">
       <h1 className="app__title">Simple To Do</h1>
+      <List />
     </div>
   )
 }
---
+++ src/components/List/List.tsx
@@ -0,0 +1,11 @@
+import './List.scss';
+
+function List() {
+  return (
+    <div className="list">
+      <input type="text" className="list__input" placeholder="Что нужно сделать?"/>
+    </div>
+  )
+}
+
+export default List;
