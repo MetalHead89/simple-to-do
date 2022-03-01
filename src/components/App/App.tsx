470299552f82a4f98f9abcda8f06f2dc2d0bfedd
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-03-01 21:27:26 +0700

    Добавлен заголовок приложения

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return null;
+  return (
+    <div className="app">
+      <h1 className="app__title">Simple To Do</h1>
+    </div>
+  )
 }
 
 export default App;
