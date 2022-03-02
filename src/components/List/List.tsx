256104804e5e56604e9037e635aac6b2bbbd852b
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2022-03-02 22:15:09 +0700

    Организовано добавление элементов в список

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,11 +1,35 @@
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { ListSlice } from '../../store/reducers/ListSlice';
 import './List.scss';
 
 function List() {
+  const { list } = useAppSelector((state) => state.listReducer);
+  const { addItem } = ListSlice.actions;
+  const dispatch = useAppDispatch();
+
+  const items = list.map((item) => <li key={item.id}>{item.text}</li>);
+
+  const handleListKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const target = event.target;
+
+    if (event.key === 'Enter' && target instanceof HTMLInputElement) {
+      dispatch(addItem(target.value));
+      
+      target.value = '';
+    }
+  };
+
   return (
     <div className="list">
-      <input type="text" className="list__input" placeholder="Что нужно сделать?"/>
+      <input
+        type="text"
+        className="list__input"
+        placeholder="Что нужно сделать?"
+        onKeyDown={(event) => handleListKeyDown(event)}
+      />
+      <ul>{items}</ul>
     </div>
-  )
+  );
 }
 
 export default List;
