a5bbdbfab1dfe50cf564d58552df26153b38298f
Bakalov Vitaliy <bakalov.vitaliy@gmail.com>
2023-08-13 13:33:21 +0700

    Added styles assests and install normalize.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,26 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
+  components: [
+    '~/components',
+    '~/components/ui'
+  ],
+
   modules: [
     'nuxt-vitest',
     '@pinia/nuxt'
-  ]
+  ],
+
+  css: ['@/assets/styles/main.scss'],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData:
+            '@use "@/assets/styles/variables.scss" as *;'
+        }
+      }
+    }
+  }
 })
